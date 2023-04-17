c008c1a3ee92048efe1a5da8599592443f0fe7f5
Diego Gonzalez <diego.g.s@hotmail.com>
2023-04-17 11:57:45 -0600

    Delete E-Commerce-Data-Pipeline directory

--- E-Commerce-Data-Pipeline/queries/global_ammount_order_status.sql
+++
@@ -1,7 +0,0 @@
--- TODO: This query will return a table with two columns; order_status, and
--- Ammount. The first one will have the different order status classes and the
--- second one the total ammount of each.
-
-SELECT olist_orders.order_status, COUNT(olist_orders.order_status) AS Ammount
-FROM olist_orders
-GROUP BY olist_orders.order_status
